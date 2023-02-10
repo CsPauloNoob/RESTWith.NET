b0c471b341c6e0be3aae585055c157d2a53fc2f2
CsPauloNoob <ps616131@gmail.com>
2023-02-09 22:13:19 -0500

    Add Person COntroller and Model Person

---
+++ RESTWithDotNet/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace RESTWithDotNet.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public long LastName { get; set; }
+
+        public int Address { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
